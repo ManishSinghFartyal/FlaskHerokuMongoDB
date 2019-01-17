eb57ffca0c045b65708610c17201278c701504d5
Manish Singh <manishfartyal9090@gmail.com>
2019-01-17 15:13:27 +0530

    Update db.py

--- tests/db.py
+++ tests/db.py
@@ -1,6 +1,12 @@
 import pymongo
 def get_db():
-	myclient=pymongo.MongoClient("mongodb://localhost:27017")
-	mydb=myclient['GitHub']
-	mycol=mydb['user_details']
-	return mycol
+	DB_NAME = 'user_details'
+	DB_HOST = 'ds159164.mlab.com'
+	DB_PORT = 59164
+	DB_USER = 'ManishSinghFartyal'
+	DB_PASS = 'Mf@#$2211'
+	connection = pymongo.MongoClient(DB_HOST, DB_PORT)
+	db = connection['user_details']
+	db.authenticate(DB_USER, DB_PASS)
+	kyc=db['kyc']
+	return kyc
